575b90a141e8d6eee9f6214001873624356539af
Brad Rutkowski <rutkowski.bradley@gmail.com>
2023-04-06 23:38:41 -0400

    Tweaks... WIP

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,11 @@ function App() {
     <div className="App">
       {/* <header className='App-header'></header> */}
       <div className="body-content">
-        <h5>Everyone deserves affordable access to quality <span className='text-highlight'>mental health care</span>...</h5>
-        <p>...but it's not always easy to find.<br/>
-          If you want the support of a therapist or counselor but aren't able to afford the cost, consider
-          looking into <span className='text-highlight'><b>official training clinics</b></span> near you.</p>
+        <h5>Everyone deserves access to <span className='text-highlight'>affordable</span> and <span className='text-highlight'>quality mental health care</span>...</h5>
+        <p style={{ textAlign: 'center' }}>...but we know that it's not always easy to find.</p><br/>
+        <p>If you are seeking the support of a therapist or counselor but aren't able to afford the cost, consider
+          looking into <span className='text-highlight'><b>official APTC training clinics</b></span> near you.</p>
+        <p>These clinics, officially recognized by the Association of Psychology Training Clinics, are typically universities...</p>
         <Map />
 
         {/* Footer */}
---
+++ src/scripts/parser.ts
@@ -0,0 +1,15 @@
+// const fs = require("fs");
+
+// // import * as fs from "fs";
+// // import * as path from "path";
+
+// // import { OrgInfo } from "../common/types";
+
+// function parseTrainingClinicCSV() {
+//   fs.readFile('../../aptc-training-clinics.csv', (err: string, inputD: any) => {
+//     if (err) throw err;
+//     console.log(inputD.toString());
+//  })
+// }
+
+export {}

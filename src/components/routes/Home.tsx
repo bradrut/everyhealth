28bfcb2ec63fb8725ccbfd36787b26f30baf4017
Brad Rutkowski <rutkowski.bradley@gmail.com>
2023-04-17 13:05:21 -0400

    Added button to homepage content for easier access to the map. Styling tweaks.

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -1,4 +1,5 @@
-import '../../App.css';
+import { Button, Group } from '@mantine/core';
+
 import './Home.css';
 import Map from '../Map';
 import { COLORS } from '../../common/colors';
@@ -15,10 +16,19 @@ export function Home() {
         <h3 className='heading-text'><span style={TEXT_HIGHLIGHT_STYLE}>Therapy</span> and <span style={TEXT_HIGHLIGHT_STYLE}>counseling</span> should be affordable for <span style={TEXT_HIGHLIGHT_STYLE}>everyone.</span></h3>
         <img src={TherapyImage} className='therapy-img' alt="Psychologist providing mental health services and counselling with psychotherapy"></img>
       </div>
-      <p>...and that's why we have created the <a href={'#trainingClinicsMap'} rel="noopener noreferrer" style={{ color: COLORS.primaryHighlight, textDecoration: 'none' }}>EveryHealth training clinics map</a> &mdash; a mental health resource to help you find affordable mental heath care near you.</p>
-      <p>If you are considering seeking the support of a therapist or counselor but the cost seems insurmountable, consider
-        looking into <b>official APTC training clinics</b> near you.</p>
-      <p>These clinics, officially recognized by the <a href={'https://www.aptc.org/'} target="_blank" rel="noopener noreferrer" style={{ color: COLORS.primaryHighlight }}>Association of Psychology Training Clinics (APTC)</a>, are typically universities...</p>
+      <div className='home-text-content-wrapper'>
+        <p style={{ marginBottom: '0px', textAlign: 'center', maxWidth: '60vw', margin: 'auto' }}>...and that's why we have created the <span style={{ color: COLORS.primaryHighlight }}>EveryHealth training clinics map</span> &mdash; a mental health resource to help you find affordable mental heath care.</p>
+        <Group position="center" mt="xl">
+          <a href={'#trainingClinicsMap'} rel="noopener noreferrer">
+            <Button className='jump-to-map-btn' type="submit" size="md">
+              Find training clinics near you
+            </Button>
+          </a>
+        </Group>
+        <p>If you are considering seeking the support of a therapist or counselor but the cost seems insurmountable, consider
+          looking into <b>official APTC training clinics</b> near you.</p>
+        <p>These clinics, officially recognized by the <a href={'https://www.aptc.org/'} target="_blank" rel="noopener noreferrer" style={{ color: COLORS.primaryHighlight }}>Association of Psychology Training Clinics (APTC)</a>, are typically universities...</p>
+      </div>
       <Map orgData={APTC_TRAINING_CLINICS} />
     </div>
   );
